e2f96780917b92299167afde048f09b9be6f6558
Fernando Sanchez <fernandosanchez23fer@gmail.com>
2026-07-02 23:38:51 -0300

    add base route

--- src/server.js
+++ src/server.js
@@ -34,3 +34,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
+app.get("/", (req, res) => {
+  res.json({ message: "API funcionando 🚀" });
+});
