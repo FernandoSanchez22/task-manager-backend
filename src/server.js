c2b2e64f3fc1dae06b77f49aaa452d5773c2bd9f
Fernando Sanchez <fernandosanchez23fer@gmail.com>
2026-07-02 23:41:12 -0300

    fix server structure

--- src/server.js
+++ src/server.js
@@ -1,39 +1,40 @@
 require("dotenv").config();
 
-const taskRoutes=require("./routes/taskRoutes");
-const categoryRoutes = require("./routes/categoryRoutes");
 const express = require("express");
 const cors = require("cors");
 
 const connectDB = require("./config/db");
+
+const taskRoutes = require("./routes/taskRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
 const userRoutes = require("./routes/userRoutes");
-const app = express();
 
 const errorHandler = require("./middleware/errorHandler");
 
-app.use(errorHandler);
-
-// Conectar a MongoDB
-connectDB();
+const app = express();
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
 
+// Conectar DB
+connectDB();
+
+// Rutas
 app.use("/api/users", userRoutes);
 app.use("/api/categories", categoryRoutes);
-app.use("/api/tasks",taskRoutes);
+app.use("/api/tasks", taskRoutes);
 
-// Ruta de prueba
+// Ruta base (TEST)
 app.get("/", (req, res) => {
-    res.send("🚀 API Task Manager funcionando correctamente");
+  res.json({ message: "API funcionando 🚀" });
 });
+
+// Error handler (SIEMPRE al final)
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
-app.get("/", (req, res) => {
-  res.json({ message: "API funcionando 🚀" });
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
